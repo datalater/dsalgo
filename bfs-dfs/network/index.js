6fe01a91e163a04ca733a8f96e0e0e26c6060714
datalater <the7mincheol@gmail.com>
2022-03-15 15:41:39 +0900

    네트워크 재풀이

--- bfs-dfs/network/index.js
+++ bfs-dfs/network/index.js
@@ -1,28 +1,26 @@
 function solution(n, computers) {
   let count = 0;
-  const isVisited = Array(n).fill(false);
+
+  const visited = Array(n).fill(false);
+
+  function dfs(i) {
+    visited[i] = true;
+
+    for (let j = 0; j < n; j += 1) {
+      if (!visited[j] && computers[i][j]) {
+        dfs(j);
+      }
+    }
+  }
 
   for (let i = 0; i < n; i += 1) {
-    // 현재 노드를 방문하지 않았다면
-    if (!isVisited[i]) {
-      dfs(i); // 현재 노드를 탐색한다
-      count++; // 현재 노드의 dfs 탐색이 끝났으므로 네트워크 개수를 증가시킨다
+    if (!visited[i]) {
+      dfs(i);
+      count++;
     }
   }
 
   return count;
-
-  function dfs(node) {
-    isVisited[node] = true;
-
-    for (let i = 0; i < n; i += 1) {
-      // 인접노드가 있고 방문하지 않았다면
-      if (computers[node][i] && !isVisited[i]) {
-        // 인접노드를 계속해서 탐색한다
-        dfs(i);
-      }
-    }
-  }
 }
 
 export { solution };
